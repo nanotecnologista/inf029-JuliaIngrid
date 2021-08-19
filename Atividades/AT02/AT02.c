50dff36cfa9c39a2fbb4124aef130b10e0f7a092
JuLinux <julia.ingridsantos.7@gmail.com>
2021-08-19 19:31:26 -0300

    Calculadora pronta

--- Atividades/AT02/AT02.c
+++ Atividades/AT02/AT02.c
@@ -2,5 +2,59 @@
 #include <math.h>
 
 int main (){
-    int somar=0, dividir=0, sair=0, subtrair=0, multiplicar=0;  
+    int  escolha=0, continua=1, contadora=0;
+    float valor=0, resultado=0;
+
+    printf("\tCalculadora teste\n");
+
+    while (continua==1){
+
+        printf("\t  MENU:\n[0]Sair       [1]Somar\n[2]Subtrair   [3]Multiplicar\n[4]Dividir\n");
+        scanf("%d", &escolha);
+
+        if (escolha!=0 && contadora==0){
+            printf("Insira o primeiro valor:\n");
+            scanf("%f", &resultado);
+        }
+            
+        switch (escolha){
+            case 1: //Soma
+                printf("Somando\n");
+                scanf("%f", &valor);
+                resultado+=valor;
+                printf("o Resultado é: %.2f\n", resultado);
+
+            break;
+            case 2: //Subtração
+                printf("Subtraindo\n");
+                scanf("%f", &valor);
+                resultado-=valor;
+                printf("o Resultado é: %.2f\n", resultado);
+
+            break;
+            case 3: //Multiplicar
+                printf("Multiplicando\n");
+                scanf("%f", &valor);
+                resultado*=valor;
+                printf("o Resultado é: %.2f\n", resultado);
+
+            break;
+            case 4: //Dividir
+                printf("Dividindo\n");
+                scanf("%f", &valor);
+                resultado/=valor;
+                printf("o Resultado é: %.2f\n", resultado);
+
+            break;
+            default: //Qualquer outro : cancela 
+                printf("Xau!!\n");
+                printf("o Resultado final é: %.2f\n", resultado);
+                continua=0;
+
+            break;
+        }
+        contadora++;
+    }
+    
+    
 }
