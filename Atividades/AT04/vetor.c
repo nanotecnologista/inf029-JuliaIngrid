77675222dd054850d78cac35c5bd8a4da416caf9
Juju Nanotech <76189249+nanotecnologista@users.noreply.github.com>
2021-09-13 11:37:23 -0300

    Refiz a quest 4 (vetor)

--- Atividades/AT04/vetor.c
+++ Atividades/AT04/vetor.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
+// Crie um programa que tenha uma função ler3Numeros. Essa função deve ler três números do
+// usuário e retornar os três números. A função main deve imprimir esses três números. Utilize vetor
+// para fazer o retorno.
 
- /*             _
-                         / /
-               __   __  '-'__  ___     ___
-              |  | |  |   |  ||   |   |   |
-              |  | |  |   |  ||   |___|   |
-         __   |  | |  |   |  ||   '___'   |
-        |  |__|  | |   ---   ||   |   |   | 
-         \__ __ _|  \_______/ '___'   '___' */
+int *ler3numeros(){
+    static int vet[3];
+    int i=0;
+
+    for ( i = 0; i < 3; i++){
+        printf("Insira o %d valor:\n", i);
+        scanf("%d", &vet[i]);
+    }
+
+    return vet;
+}
 
 
 int main( ){
-    int num1, num2, num3;
+    int *ponteiroVetor;
 
-    printf("Insira 3 valores\n:");
-     
+    printf("Insira 3 valores: \n");
+    ponteiroVetor = ler3numeros();
+
+    for (int i = 0; i < 3; i++){
+        printf("O %d valor e %d \n", i, ponteiroVetor[i]);
+    }
+    
+    return 0;    
 }
